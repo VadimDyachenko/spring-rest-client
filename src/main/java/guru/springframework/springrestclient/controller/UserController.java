58b21f5b4bdbcde9c50b231ecb3227eb3dd864d8
Vadym Diachenko <vadim.dyachenko@gmail.com>
2019-03-17 10:41:41 +0200

    Added reactive implementation

--- src/main/java/guru/springframework/springrestclient/controller/UserController.java
+++ src/main/java/guru/springframework/springrestclient/controller/UserController.java
@@ -26,18 +26,23 @@ public class UserController {
 
     @PostMapping("/users")
     public String postForm(Model model, ServerWebExchange serverWebExchange) {
-        MultiValueMap<String, String> map = serverWebExchange.getFormData().block();
+        //Non reactive implementation
+//        MultiValueMap<String, String> map = serverWebExchange.getFormData().block();
+//
+//        Integer limit = Integer.valueOf(map.get("limit").get(0));
+//
+//        log.debug("Received Limit value: {}", limit);
+//        //default if null or zero
+//        if (limit == null || limit == 0) {
+//            log.debug("Setting limit to default of 10");
+//            limit = 10;
+//        }
+//
+//        model.addAttribute("users", apiService.getUsers(limit));
 
-        Integer limit = Integer.valueOf(map.get("limit").get(0));
-
-        log.debug("Received Limit value: {}", limit);
-        //default if null or zero
-        if (limit == null || limit == 0) {
-            log.debug("Setting limit to default of 10");
-            limit = 10;
-        }
-
-        model.addAttribute("users", apiService.getUsers(limit));
+        model.addAttribute("users",
+                apiService.getUsers(serverWebExchange.getFormData().map(data -> new Integer(data.getFirst("limit"))))
+        );
 
         return "userlist";
     }
